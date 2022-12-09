2af08a1ca25daa4e21bea960df014ce33b767dc9
Newton Mutugi <newtonmut197@gmail.com>
2022-12-10 02:23:22 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+  * main - Entry point of the program
+  *
+  * Description - Executes the program
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	
+	return (0);
+}
