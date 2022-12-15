7452eae5a6da05239290b587b4dec289fef4acc3
Newton Mutugi <newtonmut197@gmail.com>
2022-12-15 11:06:30 +0300

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - defines the prototype in mains.h
+  *
+  * Description - prints alphabet 10 times
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
