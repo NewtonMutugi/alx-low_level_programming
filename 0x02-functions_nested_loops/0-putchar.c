5254eb63b23c95ed627a1ec6516b9c77ec514632
Newton Mutugi <newtonmut197@gmail.com>
2022-12-15 09:55:04 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
-README.md
+#include <stdio.h>
+/**
+  * main - entry point of the program
+  *
+  * Description - program that outputs putchar then a new line
+  *
+  * Return: 0 for successs
+  */
+int main(void)
+{
+	char string[] = "_putchar";
+	int i = 0;
+
+	while (string[i] != '\0')
+	{
+		putchar(string[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
