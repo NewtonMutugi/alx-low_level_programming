59aed874c305a39ddd1274cdff0e04445dbc84f2
Newton Mutugi <newtonmut197@gmail.com>
2022-12-15 10:14:25 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,22 @@
-#include <stdio.h.>
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+  * print_alphabet - function
+  *
+  * Description - prints alphabet
+  *
+  * Return: void no return type
+  */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a', letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+}
+#ifndef
