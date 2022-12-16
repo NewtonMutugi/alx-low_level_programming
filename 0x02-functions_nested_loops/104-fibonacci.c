16b0e6215e3c7068d4bb2cb2522755ebbb30ce6c
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 21:13:36 +0300

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * length_of_number - returns the length of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: length of number
+ */
+
+
+int length_of_number(int n)
+{
+	int length = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		++length;
+	}
+	return (length);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * 		followed by a new line
  *
  * Return: 0 if success
  *
@@ -9,7 +33,8 @@
 
 int main(void)
 {
-	unsigned long int num1, num2, sum, i;
+	int i, length;
+	long int num1, num2, sum;
 
 	num1 = 1;
 	num2 = 2;
@@ -19,6 +44,9 @@ int main(void)
 		sum = num1 + num2;
 		num1 = num2;
 		num2 = sum;
+		length = length_of_number(sum);
+		if (length == 1)
+			printf(" ");
 		printf("%ld", sum);
 		if (i != 95)
 			printf(", ");
