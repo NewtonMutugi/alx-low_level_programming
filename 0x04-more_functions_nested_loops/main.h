cf57ac660587b6a7e53be790761a39f2516fb09c
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 23:17:46 +0300

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,21 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+void print_string(char *s);
+void print_char(char c);
+void print_array(int *a, int n);
+void print_string_2(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+int _strlen(char *s);
+
 
 
 #endif /* MAIN_H */
