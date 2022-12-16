578c2279121194f91c7bd6f6b5dda1432df6edd6
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 23:51:18 +0300

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int i = 2;
+
+	while (i < n)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%ld", n);
+
+	return (0);
+}
