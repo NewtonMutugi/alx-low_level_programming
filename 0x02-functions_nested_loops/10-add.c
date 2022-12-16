dc2f836334ac1cf2699d21417300b638f8d7fbd1
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 19:49:25 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - adds two numbers
+  *
+  * @num1: first numeber
+  * @num2: second number
+  *
+  * Return: Adds the two number an returns the number
+  */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
