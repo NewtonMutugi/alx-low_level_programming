9996d9250c390ccdf63e94d653eaebac6cb17d6a
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 22:09:16 +0300

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - checks if the integer is positive or negative
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 98 % 2;
+	positive_or_negative(n);
+
+	return (0);
+}
