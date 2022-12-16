59914864226913a83f141760a436133ac77edef9
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 20:00:08 +0300

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_to_98 - prints natural numbers
+  *
+  * @n: stores the number less than 98
+  *
+  * Return: Nothing
+  */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; n > 98; --i)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; n < 98; ++i)
+			printf("%d", ",i");
+	}
+}
