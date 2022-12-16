fbb1c4b1b979636d5a9a4b4e2d2063e60c69ed1e
Newton Mutugi <newtonmut197@gmail.com>
2022-12-16 18:36:54 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43 + 0);
+		putchar(43 + 0);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0 + 48);
+		putchar(0 + 48);
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar(45 + 0);
+		putchar(45 + 0);
 		return (-1);
 	}
 	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-printf("-");
-		return (-1);#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
