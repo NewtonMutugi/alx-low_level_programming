6e6a5b4b8d77b7a347f738448f0d6e8efd754472
Newton Mutugi <newtonmut197@gmail.com>
2022-12-20 02:52:01 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,13 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _atoi(char *s);
+int _isdigit(int c);
+
 
 #endif /* MAIN_H */
