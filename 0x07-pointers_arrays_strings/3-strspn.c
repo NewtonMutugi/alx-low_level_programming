822ce8f9d3f86ffffb6fd0d4c17134e3184bf6bf
Newton Mutugi <newtonmut197@gmail.com>
2023-01-04 11:27:43 +0300

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,13 +14,17 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i]; i++) {
-		for (j = 0; accept[j]; j++) {
-			if (s[i] == accept[j]) {
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
 				break;
 			}
 		}
-		if (!accept[j]) {
+		if (!accept[j])
+		{
 			break;
 		}
 	}
