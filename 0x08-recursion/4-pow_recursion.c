2c38c0fc4ad14ed97c07de886d62da3b9d3eb6c5
Newton Mutugi <newtonmut197@gmail.com>
2023-01-08 00:02:35 +0300

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion -  function that returns the value of x raised to the power of y
+ *
+ * @x: number
+ * @y: power
+ *
+ * Return: -1 if y is less than 0, value otherwise
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
