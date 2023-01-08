c403aea214afd088bf1b89b6223496439bfce8f3
Newton Mutugi <newtonmut197@gmail.com>
2023-01-09 00:23:01 +0300

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,6 @@
 /**
- * _pow_recursion -  function that returns the value of x raised to the power of y
+ * _pow_recursion -  function that returns the value of x
+ * raised to the power of y
  *
  * @x: number
  * @y: power
