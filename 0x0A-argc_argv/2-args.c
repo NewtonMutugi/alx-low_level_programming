9dc1e6d5debe9739f563c3381cbc1872e978a75e
Newton Mutugi <newtonmut197@gmail.com>
2023-01-12 22:07:10 +0300

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of program
+ *
+ * @argc: Number of arguments supplied
+ * @argv: array of pointers to the string
+ * Return: EXIT_SUCCESS if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+
+}
