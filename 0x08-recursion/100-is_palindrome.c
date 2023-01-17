50e56fd78cb63a8dc7f2b8b4c8c00ae28122055f
Newton Mutugi <newtonmut197@gmail.com>
2023-01-17 19:58:11 +0300

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * is_palindrome_rec - function that checks if a string is
+ * a palindrome
+ *
+ * @s: String to be tested
+ * @first: first number in string
+ * @last: second number in string
+ *
+ * Return: 1 if it is a palindrome, 0 if not
+ *
+ */
+
+int is_palindrome_rec(char *s, int first, int last)
+{
+
+	if (first == last)
+		return (1);
+	if (s[first] != s[last])
+		return (0);
+
+	if (first < last + 1)
+		return (is_palindrome_rec(s, first + 1, last - 1));
+
+	return (1);
+}
+
+
+
+
+/**
+ * is_palindrome - function that checks if a string is
+ * a palindrome
+ *
+ * @s: String to be tested
+ *
+ * Return: 1 if it is a palindrome, 0 if not
+ *
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+
+	if (length <= 1)
+		return (1);
+	if (s[0] != s[length - 1])
+		return (0);
+	return (is_palindrome_rec(s, 0, length - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 
 int _putchar(char c);
