eaeccb65569bc3567e4581cd87f80232db7241a6
Newton Mutugi <newtonmut197@gmail.com>
2023-01-18 21:20:48 +0300

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,33 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 
-int is_number(char* string) {
-    unsigned int i;
+/**
+ * is_number - checks if a number is a number
+ *
+ * @string: string to test
+ *
+ * Return: 0 if not a number, 1 if a number
+ */
 
-    for (i = 0; i < strlen(string); i++) {
-        if (!isdigit(string[i])) {
-            return 0;
-        }
-    }
-    return 1;
+int is_number(char *string)
+{
+	unsigned int i;
+
+	for (i = 0; i < strlen(string); i++)
+	{
+		if (!isdigit(string[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
 
-int main(int argc, char** argv) {
-	    int num1, num2;
-    if (argc != 3) {
-        printf("Error\n");
-        exit(98);
-    }
+/**
+ * main - Program entry point
+ *
+ * @argc: size of array
+ * @argv: character array
+ *
+ * Return: 0 on success, 98 on failure
+ */
 
-    if (!is_number(argv[1]) || !is_number(argv[2])) {
-        printf("Error\n");
-        exit(98);
-    }
+int main(int argc, char **argv)
+{
+	int num1, num2;
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    printf("%d\n", num1 * num2);
-    return 0;
+	if (!is_number(argv[1]) || !is_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
 }
 
