b1527b402bcb49798168cd4a40888a58cb4806a5
Newton Mutugi <newtonmut197@gmail.com>
2023-01-18 18:16:19 +0300

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  function that allocates memory using malloc
+ *
+ * b: size of the memory to allocate
+ *
+ * Return: pointer to the allocated memory,
+ * normal process termination with a status value of 98
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	exit(98);
+	return (p);
+}
