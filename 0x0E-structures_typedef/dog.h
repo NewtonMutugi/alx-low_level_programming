1906601497ca9ca6771a0a15ba04619d20a359c1
Newton Mutugi <newtonmut197@gmail.com>
2023-01-19 21:24:11 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,11 @@ typedef struct dog
 	char *owner;
 } dog;
 
+/**
+ * dog_t - New struct dog
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
