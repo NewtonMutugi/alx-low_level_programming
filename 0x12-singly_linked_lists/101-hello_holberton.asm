11a9953e34f70b80bf75d71c47829443391a8dc3
Newton Mutugi <newtonmut197@gmail.com>
2023-02-02 10:55:10 +0300

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,21 @@
-section .data
-    hello db 'Hello, Holberton', 0
 
-section .text
-    global _start
+	extern	printf
 
-_start:
-    ; write string to stdout
-    mov eax, 4 ; system call number (sys_write)
-    mov ebx, 1 ; file descriptor (stdout)
-    mov ecx, hello ; pointer to string to write
-    mov edx, 13 ; length of string to write
-    int 0x80 ; call kernel
+	section .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-    ; write a newline to stdout
-    mov eax, 4 ; system call number (sys_write)
-    mov ebx, 1 ; file descriptor (stdout)
-    mov ecx, newline ; pointer to newline character
-    mov edx, 1 ; length of string to write (1 byte)
-    int 0x80 ; call kernel
+	section .text
 
-    ; exit with status code 0
-    mov eax, 1 ; system call number (sys_exit)
-    xor ebx, ebx ; exit status
-    int 0x80 ; call kernel
+	global main
+main:
+	push	rbp
 
-section .rodata
-    newline db 10
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+
+	mov	rax,0
+	ret
