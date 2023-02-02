e1d2eb80e9ab313b3fb7c2e9c439296603192116
Newton Mutugi <newtonmut197@gmail.com>
2023-02-02 10:33:36 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - prints a message before the main function is executed
+ *
+ * Return: Nothing
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
