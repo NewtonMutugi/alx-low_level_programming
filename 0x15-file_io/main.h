8a8f19c4ce0953d7b5ddec7f472100bc18938876
NewtonMutugi <newtonmut197@gmail.com>
2023-02-08 17:41:14 +0300

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,15 +12,4 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int remove_file(const char *filename);
-int copy_file(const char *filename, const char *filename2);
-int move_file(const char *filename, const char *filename2);
-int rename_file(const char *filename, const char *filename2);
-int truncate_file(const char *filename, size_t length);
-int create_dir(const char *dirname);
-int remove_dir(const char *dirname);
-int change_dir(const char *dirname);
-int change_dir_back(void);
-int change_dir_home(void);
-
 #endif
