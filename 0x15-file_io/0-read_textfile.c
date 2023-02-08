539cc986e52679b43bab5bb1e8ba637a272ec7e7
NewtonMutugi <newtonmut197@gmail.com>
2023-02-08 17:37:04 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output
  * @filename: file to read
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print
- * 0 if filename is NULL or if write fails or does not write the expected amount
- * of bytes
+ * 0 if filename is NULL or if write fails or does not write the expected
+ * amountof bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
