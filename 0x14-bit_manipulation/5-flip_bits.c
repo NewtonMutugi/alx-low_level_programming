2e99268cc9678e8424051d04ffa32ebfebaf27a6
NewtonMutugi <newtonmut197@gmail.com>
2023-02-08 15:23:14 +0300

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get from one
+ * number to another
+ * @n: number
+ * @m: number
+ * Return: number of bits you would need to flip to get from one number to
+ * another
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i, count = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+			count++;
+	}
+	return (count);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,8 @@ void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int get_word(unsigned long int *n);
 
 #endif /* MAIN_H*/
