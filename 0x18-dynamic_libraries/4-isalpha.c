7dd756bac73bc214397734927a2a7292ed7ff165
NewtonMutugi <newtonmut197@gmail.com>
2023-03-21 11:47:47 +0300

    libdynamic.so

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  _isalpha - checks whether a character is an alphabet
+ *
+ *  @c: store the character to check whether it is an alphabet
+ *
+ *  Return:     1 if c is a letter
+ *
+ */
+
+char _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
