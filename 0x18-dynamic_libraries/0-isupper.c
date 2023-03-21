4e3ba3e19ca5c0a663cd2c86b080be0a81ac4324
NewtonMutugi <newtonmut197@gmail.com>
2023-03-21 12:08:20 +0300

    libdynamic.so

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - tests whether a character is uppercase
+ *
+ * @c: placeholder for the character to be tested
+ *
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset -  function fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ *
+ * @s: pointer to the variable
+ * @b: the variable to be writtem
+ * @n: the he number of bytes to be written
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
